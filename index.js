0e0dc07ff298efa110601caee1b69e3771b5e0aa
Adam <inmon.adam@yahoo.com>
2021-10-03 15:03:14 -0500

    commit tree 83f1232db1d0c002ca3c3c4efc5f70f93041dd25

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+
+function superbowlWin(array){
+    let yearWin = array.find(element => element.result === "W");
+    if (yearWin === undefined){
+        return undefined
+    }
+    else return yearWin.year;
+    
+}
+
+superbowlWin(record);
